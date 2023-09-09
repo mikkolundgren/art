c28dcdd615e788ae993c289debdb715f7621e808
Mikko Lundgren <mikko.lundgren@op.fi>
2023-09-09 11:57:27 +0300

    refactored for azure

--- handler.go
+++ handler.go
@@ -5,18 +5,63 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"bytes"
+	"image"
+	"image/color"
+	_ "image/jpeg"
 
-	"github.com/mikkolundgren/art/asciiart"
+	"io"
+	"golang.org/x/image/draw"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("Got request method %s", r.Method)
     defer r.Body.Close()
 
     r.ParseMultipartForm(32 << 20)
     file, _, _ := r.FormFile("data")
-	result, _ := asciiart.Draw(file)
+	result, _ := makeArt(file)
 
     fmt.Fprint(w, result)
+}
+
+func resize(img image.Image) image.Image {
+
+	result := image.NewRGBA(image.Rect(0, 0, 150, 200))
+	draw.ApproxBiLinear.Scale(result, result.Rect, img, img.Bounds(), draw.Over, nil)
+	return result
+
+}
+
+func makeArt(r io.Reader) (string, error) {
+	
+	img, format, err := image.Decode(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Image format: ", format)
+	fmt.Println("Original image size: ", img.Bounds())
+	resized := resize(img)
+	fmt.Println("Resized image: ", resized.Bounds())
+
+	levels := []string{" ", "░", "▒", "▓", "█"}
+
+	var b bytes.Buffer
+
+
+	for y := resized.Bounds().Min.Y; y < resized.Bounds().Max.Y; y++ {
+		for x := resized.Bounds().Min.X; x < resized.Bounds().Max.X; x++ {
+			c := color.GrayModel.Convert(resized.At(x, y)).(color.Gray)
+			level := c.Y / 51 // 51 * 5 = 255
+			if level == 5 {
+				level--
+			}
+			b.WriteString(levels[level])
+		}
+		b.WriteString("\n")
+	}
+
+	return b.String(), nil
 }
 
 func main() {
@@ -24,7 +69,7 @@ func main() {
     if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
         listenAddr = ":" + val
     }
-    http.HandleFunc("/api/AsciiArt", helloHandler)
-    log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
+    http.HandleFunc("/api/asciiart", helloHandler)
+    fmt.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
     log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
